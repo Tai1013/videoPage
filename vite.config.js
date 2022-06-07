56fedf2a446f179c5a7e3f30c206ff4b5e056bbb
Tai Fu <frank22199215@gmail.com>
2022-06-08 01:02:45 +0800

    first commit3

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const locals = { name: "My Pug" }
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),pugPlugin(options, locals)],
-  base: '/docs/',
+  base: '/videoPage/',
   build: {
     outDir: 'docs', // 構建輸出路徑
     assetsDir: 'assets', //靜態資原始檔夾，和outDir同級
